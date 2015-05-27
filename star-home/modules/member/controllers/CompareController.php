a8c8b3377f05bf4011a2defd8df0568ae207cddd
wucangzhou <wucang.zhou@jago-ag.cn>
2015-05-27 19:23:02 +0800

    compare view

--- star-home/modules/member/controllers/CompareController.php
+++ star-home/modules/member/controllers/CompareController.php
@@ -2,21 +2,21 @@
 
 namespace home\modules\member\controllers;
 
-use home\modules\member\models\Wishlist;
 use star\catalog\models\Item;
 use yii\web\Controller;
-use yii\data\Pagination;
 use yii;
 
 class CompareController extends Controller
 {
 
+    public $layout="/main";
+
     public function actionIndex()
     {
         $itemModels = Item::find()->where(['in','item_id',$_POST['itemId']])->all();
-        
+
         return $this->render('compare',[
-            'compareList' => $itemModels
+            'itemModels' => $itemModels
         ]);
     }
 }
