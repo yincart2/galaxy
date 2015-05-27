aeb34a031e3af84c06daf3927d78c4cf460f7128
changhai.lin <changhai.lin@jago-ag.cn>
2015-05-27 18:46:15 +0800

    no message

--- star-themes/star/home/modules/member/wishlist/favorite.php
+++ star-themes/star/home/modules/member/wishlist/favorite.php
@@ -10,6 +10,8 @@ $this->params['breadcrumbs'][] = [
     'label' => Yii::t('member','Favorite List'),
     'template' => '<li><span>{link}</span></li>',
 ];
+$link = $this->getAssetManager()->getPublishedUrl('@theme/star/home/assets');
+$items = \star\catalog\models\Item::find()->all();
 ?>
 <h2 class="tt_uppercase color_dark m_bottom_20">My Wishlist</h2>
 <div class="row clearfix m_bottom_15">
@@ -45,23 +47,27 @@ $this->params['breadcrumbs'][] = [
     </tr>
     </thead>
     <tbody>
+    <?php
+    /** @var \star\catalog\models\Item $item */
+    foreach($items as $item) {
+    ?>
     <tr>
         <!--product image-->
-        <td data-title="Product Image"><img src="images/quick_view_img_10.jpg" alt=""></td>
+        <td data-title="Product Image"><img src="<?= $link?>/images/quick_view_img_10.jpg" alt=""></td>
         <!--product name and category-->
         <td data-title="Product Name">
-            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5">Eget elementum vel</a><br>
-            <a href="#" class="default_t_color">Dresses</a>
+            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5"><?= $item->title ?></a><br>
+            <a href="#" class="default_t_color"><?= $item->category->name?></a>
         </td>
         <!--product price-->
         <td data-title="Price">
-            <span class="scheme_color fw_medium f_size_large">$102.00</span>
+            <span class="scheme_color fw_medium f_size_large"><?= $item->price ?></span>
         </td>
         <!--quanity-->
         <td data-title="Quantity">
             <div class="clearfix quantity r_corners d_inline_middle f_size_medium color_dark">
                 <button class="bg_tr d_block f_left" data-direction="down">-</button>
-                <input type="text" name="" readonly value="1" class="f_left">
+                <input type="text" name="" readonly value="<?= $item->stock ?>" class="f_left">
                 <button class="bg_tr d_block f_left" data-direction="up">+</button>
             </div>
         </td>
@@ -71,127 +77,10 @@ $this->params['breadcrumbs'][] = [
                 Cart
             </button>
             <br>
-            <a href="#" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
+            <a href="<?= Url::to(['/member/wishlist/delete-wishlist'])?>" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
         </td>
     </tr>
-    <tr>
-        <!--product image-->
-        <td data-title="Product Image"><img src="images/wishlist_img_1.jpg" alt=""></td>
-        <!--product name and category-->
-        <td data-title="Product Name">
-            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5">Aenean nec eros</a><br>
-            <a href="#" class="default_t_color">Skirts</a>
-        </td>
-        <!--product price-->
-        <td data-title="Price">
-            <span class="scheme_color fw_medium f_size_large">$52.00</span>
-        </td>
-        <!--quanity-->
-        <td data-title="Quantity">
-            <div class="clearfix quantity r_corners d_inline_middle f_size_medium color_dark">
-                <button class="bg_tr d_block f_left" data-direction="down">-</button>
-                <input type="text" name="" readonly value="1" class="f_left">
-                <button class="bg_tr d_block f_left" data-direction="up">+</button>
-            </div>
-        </td>
-        <!--add or remove buttons-->
-        <td data-title="Action">
-            <button class="button_type_4 bg_scheme_color r_corners tr_all_hover color_light mw_0 m_bottom_10">Add to
-                Cart
-            </button>
-            <br>
-            <a href="#" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
-        </td>
-    </tr>
-    <tr>
-        <!--product image-->
-        <td data-title="Product Image"><img src="images/wishlist_img_2.jpg" alt=""></td>
-        <!--product name and category-->
-        <td data-title="Product Name">
-            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5">Ut tellus dolor
-                dapibus</a><br>
-            <a href="#" class="default_t_color">Pants</a>
-        </td>
-        <!--product price-->
-        <td data-title="Price">
-            <span class="scheme_color fw_medium f_size_large">$360.00</span>
-        </td>
-        <!--quanity-->
-        <td data-title="Quantity">
-            <div class="clearfix quantity r_corners d_inline_middle f_size_medium color_dark">
-                <button class="bg_tr d_block f_left" data-direction="down">-</button>
-                <input type="text" name="" readonly value="1" class="f_left">
-                <button class="bg_tr d_block f_left" data-direction="up">+</button>
-            </div>
-        </td>
-        <!--add or remove buttons-->
-        <td data-title="Action">
-            <button class="button_type_4 bg_scheme_color r_corners tr_all_hover color_light mw_0 m_bottom_10">Add to
-                Cart
-            </button>
-            <br>
-            <a href="#" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
-        </td>
-    </tr>
-    <tr>
-        <!--product image-->
-        <td data-title="Product Image"><img src="images/wishlist_img_3.jpg" alt=""></td>
-        <!--product name and category-->
-        <td data-title="Product Name">
-            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5">Vestibulum sed ante</a><br>
-            <a href="#" class="default_t_color">Tops</a>
-        </td>
-        <!--product price-->
-        <td data-title="Price">
-            <span class="scheme_color fw_medium f_size_large">$85.00</span>
-        </td>
-        <!--quanity-->
-        <td data-title="Quantity">
-            <div class="clearfix quantity r_corners d_inline_middle f_size_medium color_dark">
-                <button class="bg_tr d_block f_left" data-direction="down">-</button>
-                <input type="text" name="" readonly value="1" class="f_left">
-                <button class="bg_tr d_block f_left" data-direction="up">+</button>
-            </div>
-        </td>
-        <!--add or remove buttons-->
-        <td data-title="Action">
-            <button class="button_type_4 bg_scheme_color r_corners tr_all_hover color_light mw_0 m_bottom_10">Add to
-                Cart
-            </button>
-            <br>
-            <a href="#" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
-        </td>
-    </tr>
-    <tr>
-        <!--product image-->
-        <td data-title="Product Image"><img src="images/wishlist_img_4.jpg" alt=""></td>
-        <!--product name and category-->
-        <td data-title="Product Name">
-            <a href="#" class="fw_medium d_inline_b f_size_ex_large color_dark m_bottom_5">Nemo enim ipsam
-                voluptatem</a><br>
-            <a href="#" class="default_t_color">Tops</a>
-        </td>
-        <!--product price-->
-        <td data-title="Price">
-            <span class="scheme_color fw_medium f_size_large">$17.00</span>
-        </td>
-        <!--quanity-->
-        <td data-title="Quantity">
-            <div class="clearfix quantity r_corners d_inline_middle f_size_medium color_dark">
-                <button class="bg_tr d_block f_left" data-direction="down">-</button>
-                <input type="text" name="" readonly value="1" class="f_left">
-                <button class="bg_tr d_block f_left" data-direction="up">+</button>
-            </div>
-        </td>
-        <!--add or remove buttons-->
-        <td data-title="Action">
-            <button class="button_type_4 bg_scheme_color r_corners tr_all_hover color_light mw_0 m_bottom_10">Add to
-                Cart
-            </button>
-            <br>
-            <a href="#" class="color_dark"><i class="fa fa-times m_right_5"></i> Remove</a>
-        </td>
-    </tr>
+    <?php } ?>
     </tbody>
 </table>
 <hr class="m_bottom_10 divider_type_3">
