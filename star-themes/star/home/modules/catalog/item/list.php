5132040e017dbfcfb2440725343ba3650b47a3d5
changhai.lin <changhai.lin@jago-ag.cn>
2015-05-29 19:31:06 +0800

    no message

--- star-themes/star/home/modules/catalog/item/list.php
+++ star-themes/star/home/modules/catalog/item/list.php
@@ -215,20 +215,6 @@ foreach($items as $key=>$item) {
 <hr class="m_bottom_10 divider_type_3">
 <div class="row clearfix m_bottom_15 m_xs_bottom_30">
     <div class="col-lg-7 col-md-7 col-sm-8 m_xs_bottom_10">
-        <p class="d_inline_middle f_size_medium d_xs_block m_xs_bottom_5">Results 1 - 5 of 45</p>
-
-        <p class="d_inline_middle f_size_medium m_left_20 m_xs_left_0">Show:</p>
-        <!--show items per page select-->
-        <div class="custom_select f_size_medium relative d_inline_middle m_left_5">
-            <div class="select_title r_corners relative color_dark">9</div>
-            <ul class="select_list d_none"></ul>
-            <select name="show_second">
-                <option value="Manufacture 1">6</option>
-                <option value="Manufacture 2">3</option>
-                <option value="Manufacture 3">1</option>
-            </select>
-        </div>
-        <p class="d_inline_middle f_size_medium m_left_5">items per page</p>
     </div>
     <div class="col-lg-5 col-md-5 col-sm-4 t_align_r t_xs_align_l">
         <!--pagination-->
@@ -239,6 +225,7 @@ foreach($items as $key=>$item) {
             'options' => [
                 'class' => 'horizontal_list clearfix d_inline_middle f_size_medium m_left_10'
             ],
+            'activePageCssClass' => 'color_dark',
             'linkOptions' => [
                 'class' => 'm_right_10 m_left_10'
             ]
