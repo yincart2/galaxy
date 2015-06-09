6e363e10c3d271d189b303efd8aee204e54dd680
wucangzhou <wucang.zhou@jago-ag.cn>
2015-06-09 20:47:19 +0800

    create coupon model

---
+++ star-modules/coupon/models/Coupon.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace star\coupon\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "coupon".
+ *
+ * @property integer $coupon_id
+ * @property string $coupon_no
+ * @property integer $rule_id
+ * @property integer $order_id
+ * @property integer $user_id
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $start_at
+ * @property integer $end_at
+ * @property integer $star_id
+ */
+class Coupon extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'coupon';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['coupon_id', 'coupon_no', 'rule_id', 'created_at', 'updated_at', 'start_at', 'end_at'], 'required'],
+            [['coupon_id', 'rule_id', 'order_id', 'user_id', 'status', 'created_at', 'updated_at', 'start_at', 'end_at', 'star_id'], 'integer'],
+            [['coupon_no'], 'string', 'max' => 225]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'coupon_id' => Yii::t('coupon', 'Coupon ID'),
+            'coupon_no' => Yii::t('coupon', 'Coupon No'),
+            'rule_id' => Yii::t('coupon', 'Rule ID'),
+            'order_id' => Yii::t('coupon', 'Order ID'),
+            'user_id' => Yii::t('coupon', 'User ID'),
+            'status' => Yii::t('coupon', 'Status'),
+            'created_at' => Yii::t('coupon', 'Created At'),
+            'updated_at' => Yii::t('coupon', 'Updated At'),
+            'start_at' => Yii::t('coupon', 'Start At'),
+            'end_at' => Yii::t('coupon', 'End At'),
+            'star_id' => Yii::t('coupon', 'Star ID'),
+        ];
+    }
+}
---
+++ star-modules/coupon/models/CouponRule.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace star\coupon\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "coupon_rule".
+ *
+ * @property integer $rule_id
+ * @property string $desc
+ * @property string $condition
+ * @property string $result
+ */
+class CouponRule extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'coupon_rule';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['rule_id', 'result'], 'required'],
+            [['rule_id'], 'integer'],
+            [['desc', 'condition', 'result'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'rule_id' => Yii::t('coupon', 'Rule ID'),
+            'desc' => Yii::t('coupon', 'Desc'),
+            'condition' => Yii::t('coupon', 'Condition'),
+            'result' => Yii::t('coupon', 'Result'),
+        ];
+    }
+}
