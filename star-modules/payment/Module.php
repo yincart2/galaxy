0c6940fbb08fccd61f3ad2d71ac14ea464081ebf
wucangzhou <wucang.zhou@jago-ag.cn>
2015-06-19 14:46:34 +0800

    wechat lib

---
+++ star-modules/payment/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace star\payment;
+
+
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here
+    }
+} 
---
+++ star-modules/payment/lib/wechat/WxPayException.php
@@ -0,0 +1,14 @@
+<?php
+namespace star\payment\libl\weChat;
+/**
+ * 
+ * 微信支付API异常类
+ * @author widyhu
+ *
+ */
+class WxPayException extends \Exception {
+	public function errorMessage()
+	{
+		return $this->getMessage();
+	}
+}
