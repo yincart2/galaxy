22bb14384eaac7ee497f17ccd85f34eade31a0c0
changhai.lin <changhai.lin@jago-ag.cn>
2015-06-30 20:12:47 +0800

    no message

--- star-modules/system/models/Setting.php
+++ star-modules/system/models/Setting.php
@@ -35,7 +35,7 @@ class Setting extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [[ 'menu_code', 'menu_label', 'group_code', 'group_label','menu_sort','group_sort'], 'required'],
+            [['menu_code', 'menu_label', 'group_code', 'group_label', 'menu_sort', 'group_sort'], 'required'],
             [['setting_id', 'menu_sort', 'group_sort'], 'integer'],
             [['menu_code', 'menu_label', 'group_code', 'group_label'], 'string', 'max' => 255]
         ];
@@ -57,18 +57,20 @@ class Setting extends \yii\db\ActiveRecord
         ];
     }
 
-    public function afterSave($insert, $changedAttributes){
+    public function afterSave($insert, $changedAttributes)
+    {
         $this->saveSettingFields($this->setting_id);
         parent::afterSave($insert, $changedAttributes);
     }
 
-    public function saveSettingFields($settingId){
+    public function saveSettingFields($settingId)
+    {
         if (isset($_POST['SettingFields'])) {
             $settingFields = $_POST['SettingFields'];
             unset($_POST['SettingFields']);
             if (is_array($settingFields['type']) && $count = count($settingFields['type'])) {
                 for ($i = 0; $i < $count; $i++) {
-                    if(isset($settingFields['setting_fields_id'][$i]) && $settingFields['setting_fields_id'][$i]) {
+                    if (isset($settingFields['setting_fields_id'][$i]) && $settingFields['setting_fields_id'][$i]) {
                         $propValue = Yii::createObject(SettingFields::className());
                         $settingFieldsModel = $propValue::find()->where(['setting_fields_id' => $settingFields['setting_fields_id'][$i]])->one();;
                     } else {
@@ -80,14 +82,14 @@ class Setting extends \yii\db\ActiveRecord
                         'fields_code' => $settingFields['fields_code'][$i],
                         'fields_label' => $settingFields['fields_label'][$i],
                         'value' => $settingFields['value'][$i],
-                        'setting_code' => $this->menu_code.'_'.$this->group_code.'_'. $settingFields['fields_code'][$i],
+                        'setting_code' => $this->menu_code . '_' . $this->group_code . '_' . $settingFields['fields_code'][$i],
                         'status' => 1,
                     ));
-                    if(isset($settingFields['setting_fields_id'][$i]) && $settingFields['setting_fields_id'][$i]) {
+                    if (isset($settingFields['setting_fields_id'][$i]) && $settingFields['setting_fields_id'][$i]) {
                         $settingFieldsModel->update();
                     } else {
-                        if(!$settingFieldsModel->save()){
-                            throw new Exception(Yii::t('system','save attributes fail'));
+                        if (!$settingFieldsModel->save()) {
+                            throw new Exception(Yii::t('system', 'save attributes fail'));
                         }
                     }
                     $settingFields['setting_fields_id'][$i] = $settingFieldsModel->setting_fields_id;
@@ -106,15 +108,16 @@ class Setting extends \yii\db\ActiveRecord
                     $propValueModel::deleteAll('setting_fields_id IN (' . implode(', ', $delArr) . ')');
                 }
             }
-        }else{
+        } else {
             //已经没有属性了，要清除数据表内容
             $propValueModel = Yii::createObject(SettingFields::className());
             $propValueModel::deleteAll('setting_id = ' . $settingId);
         }
     }
 
-    public function getSettingFields(){
-        return self::hasMany(SettingFields::className(),['setting_id'=>'setting_id']);
+    public function getSettingFields()
+    {
+        return self::hasMany(SettingFields::className(), ['setting_id' => 'setting_id']);
     }
 
     public function getSystemConfig()
@@ -124,19 +127,35 @@ class Setting extends \yii\db\ActiveRecord
         $systemConfig = [];
 
         /** @var \star\system\models\Setting $setting */
-        foreach($settings as $setting) {
+        foreach ($settings as $setting) {
             $fieldsConfig = [];
             $groupConfig = [];
 
             $settingFields = $setting->settingFields;
             /** @var \star\system\models\SettingFields $settingField */
-            foreach($settingFields as $settingField) {
-                $fieldsConfig[$settingField->fields_code] = [
-                    'label' => $settingField->fields_label,
-                    'inputType' => $settingField->type,
-                    'value' => $settingField->value,
-                    'setting_code' => $settingField->setting_code
-                ];
+            foreach ($settingFields as $settingField) {
+                if ($settingField->type == 2 || $settingField->type == 3) {
+                    $data = [];
+                    $data[$settingField->value] = $settingField->value;
+
+                    if (isset($fieldsConfig[$settingField->setting_code])) {
+                        $data = ArrayHelper::merge($fieldsConfig[$settingField->setting_code]['value'], $data);
+                    }
+                    $fieldsConfig[$settingField->setting_code] = [
+                        'label' => $settingField->fields_label,
+                        'inputType' => $settingField->type,
+                        'value' => $data,
+                        'setting_code' => $settingField->setting_code
+                    ];
+
+                } else {
+                    $fieldsConfig[$settingField->setting_code] = [
+                        'label' => $settingField->fields_label,
+                        'inputType' => $settingField->type,
+                        'value' => $settingField->value,
+                        'setting_code' => $settingField->setting_code
+                    ];
+                }
             }
 
             $groupConfig[$setting->group_code] = [
@@ -145,7 +164,7 @@ class Setting extends \yii\db\ActiveRecord
                 'fields' => $fieldsConfig
             ];
 
-            if(isset($systemConfig[$setting->menu_code]) && $systemConfig[$setting->menu_code]) {
+            if (isset($systemConfig[$setting->menu_code]) && $systemConfig[$setting->menu_code]) {
                 $preConfig = $systemConfig[$setting->menu_code];
             }
             $systemConfig[$setting->menu_code] = [
@@ -153,7 +172,7 @@ class Setting extends \yii\db\ActiveRecord
                 'sort' => $setting->menu_sort,
                 'groups' => $groupConfig
             ];
-            if(isset($preConfig)) {
+            if (isset($preConfig)) {
                 $systemConfig[$setting->menu_code] = ArrayHelper::merge($preConfig, $systemConfig[$setting->menu_code]);
             }
         }
@@ -176,6 +195,7 @@ class Setting extends \yii\db\ActiveRecord
         }
 
         $config = $this->getSystemConfig();
+
         $tabItems = [];
         foreach ($config as $tabKey => $tab) {
             $groupItems = [];
@@ -183,15 +203,8 @@ class Setting extends \yii\db\ActiveRecord
                 $groupContent = '';
                 $dataList = [];
                 foreach ($group['fields'] as $fieldKey => $field) {
-                    if (isset($field['depend'])) {
-                        list($dependKey, $dependValue) = $field['depend'];
-                        $dataOptions = [
-                            'depend-key' => $dependKey,
-                            'depend-value' => $dependValue,
-                        ];
-                    }
                     $options['template'] = "<label class = \"control-label col-sm-1\">" . $field['label'] . "</label>\n<div class=\"col-sm-11\">{input}\n<div style=\"width:60%\">{hint}\n</div>{error}</div>";
-                    $fieldOptions = array_merge($options, ['options' => ['class' => 'form-group', 'data' => $dataOptions]]);
+                    $fieldOptions = array_merge($options, ['options' => ['class' => 'form-group']]);
 
                     /** @var \yii\bootstrap\ActiveField $activeField */
                     $fieldClass = Yii::createObject(SettingFields::className());
@@ -199,10 +212,10 @@ class Setting extends \yii\db\ActiveRecord
 
                     switch ($field['inputType']) {
                         case 3:
-                            $activeField->inline()->checkboxList($dataList);
+                            $activeField->inline()->checkboxList($field['value'], $fieldOptions);
                             break;
                         case 2:
-                            $activeField->inline()->radioList($dataList);
+                            $activeField->inline()->radioList($field['value'], $fieldOptions);
                             break;
                         case 1:
                             $activeField->textInput();
